4b14835478d82b250688d0211baf07e8a820315c
Lycoris_i <some.chicken@gmail.com>
2013-07-29 17:54:14 +0900

    081 first commit

---
+++ 081-090/081.rb
@@ -0,0 +1,33 @@
+# http://projecteuler.net/problem=81
+#
+#
+
+def matrix_search(matrix)
+	f = open(matrix)
+	m = f.read.split(/\n/)
+	m.each_index {|i| m[i] = m[i].split(",")}
+	m.each {|n|
+		n.each_index {|p|
+			n[p] = n[p].to_i
+		}
+	}
+	m.each_index {|v|
+		m[v].each_index {|h|
+			if v - 1 > -1 and h - 1 > -1 then
+				m[v][h] += [m[v - 1][h], m[v][h - 1]].sort.first
+			elsif v - 1 < 0  and h - 1 > -1  then
+				m[v][h] += m[v][h - 1]
+			elsif h - 1 < 0 and v - 1 > -1 then
+				m[v][h] += m[v - 1][h]
+			end
+		}
+		p m[v]
+	}
+	answer = m[m.size - 1].last
+	return answer
+end
+
+
+
+
+	puts "Answer: #{matrix_search("matrix.txt")}"	# Answer: 427337
