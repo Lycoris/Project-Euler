c81dfb39ddb9c6f592a21480e08dbefbd76672b6
Lycoris_i <some.chicken@gmail.com>
2013-07-29 16:54:16 +0900

    099 - each used

--- 091-100/099.rb
+++ 091-100/099.rb
@@ -5,16 +5,16 @@
 def Largest_exponential()
 	f = open("base_exp.txt") 
 	a= f.read.split(/\n/)
-	for i in 0..(a.size - 1)
+	a.each_index{|i|
 		a[i] = a[i].split(",")
-		for j in 0..1
+		a[i].each_index{|j|
 			a[i][j] = a[i][j].to_i
-		end
-	end
+		}
+	}
 	b = Array.new
-	for i in 0..(a.size - 1)
-		b[i] = a[i][1] * Math.log10(a[i][0])
-	end
+		a.each_index{|i|
+			b[i] = a[i][1] * Math.log10(a[i][0])
+		}
 	max = b.sort.last
 	return b.index(max) + 1
 end
