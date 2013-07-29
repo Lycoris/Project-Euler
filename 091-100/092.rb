d8e52a3744f5d07501ad98f0a53d4706c17abac6
Lycoris_i <some.chicken@gmail.com>
2013-07-29 15:08:00 +0900

    092 first commit

---
+++ 091-100/092.rb
@@ -0,0 +1,71 @@
+# http://projecteuler.net/problem=92
+#
+#
+
+
+def square_digit_chains(num)
+	t0 = Time.now
+	@database = Hash.new
+	make_database(num)
+	a = database_search(num)
+	t1 = Time.now
+	puts "#{t1 - t0} sec."
+	return a
+end
+
+
+def make_database(n)
+	@max = 0
+	for i in 0..(Math.log10(n))
+		@max += ((n / (10 ** i)) % 10) ** 2
+	end
+	for i in 1..@max
+		x = i
+		while 0 < 1
+			a = 0
+			for j in 0..(Math.log10(x))
+				a += ((x / (10 ** j)) % 10) ** 2
+			end
+			if a == 89 then
+				@database[i] = true
+				break
+			elsif a == 1 then
+				@database[i] = false
+				break
+			elsif a < i then
+				@database[i] = @database[a]
+				break
+			end
+			x = a
+		end
+	end
+	p "Making database is finished."
+end
+
+
+def database_search(n)
+	a = 0
+	for i in 1..n
+		x = 0
+		if i < @max + 1 then
+			if @database[i] == true then
+				a += 1
+				p i
+				p @database[i]
+				next
+			end
+		end
+		for j in 0..(Math.log10(i))
+			x += ((i / (10 ** j)) % 10) ** 2
+		end
+		if @database[x] == true then
+			a += 1
+		end
+		p i
+		p @database[x]
+	end
+	return a
+end
+
+
+puts "Answer:#{square_digit_chains(9999999)}"		# Answer: Answer:8581146
