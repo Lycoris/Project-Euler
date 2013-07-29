dd53d0372c1809a20d5ebad6f21cd7a195d78d29
Lycoris_i <some.chicken@gmail.com>
2013-07-30 01:15:35 +0900

    112 first commit

---
+++ 111-120/112.rb
@@ -0,0 +1,43 @@
+# http://projecteuler.net/problem=112
+#
+#
+
+
+def bouncy_numbers(r)
+	r = r.to_f / 100
+	n = 0
+	a = 0
+	rate = 0
+	until rate > r or rate == r
+		n += 1
+		if bouncy_number?(n) == true then
+			a += 1
+		end
+		rate = a.to_f / n.to_f
+		p rate
+	end
+	return n
+end
+
+
+def bouncy_number?(n)
+	a = n.to_s.split(//)
+	judge = {:increasing => true, :decreasing => true}
+	a.each_index {|i| a[i] = a[i].to_i}
+	a.each_index {|i|
+		if i != 0 then
+			if a[i] > a[i - 1] then
+				judge[:decreasing] = false
+			elsif a[i] < a[i - 1] then
+				judge[:increasing] = false
+			end
+		end
+		if judge[:increasing] == false and judge[:decreasing] == false then
+			return true
+		end
+	}
+	return false
+end
+
+
+	puts "Answer: #{bouncy_numbers(99)}"	# Answer: 1587000
