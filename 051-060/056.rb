f8938b03c1773ddbabe81b0d0a460730f18080d5
Lycoris_i <some.chicken@gmail.com>
2013-08-02 19:34:27 +0900

    056 times used

--- 051-060/056.rb
+++ 051-060/056.rb
@@ -4,23 +4,23 @@
 
 def max_digit_sum(a,b)
 	max = 0
-	for i in 1..a
-		for j in 1..b
-			n = i ** j
+	a.times {|i|
+		b.times {|j|
+			n = (i + 1) ** (j + 1)
 			if digit_sum(n) > max then
 				max = digit_sum(n)
 			end
-		end
-	end
+		}
+	}
 	return max
 end
 
 def digit_sum(num)
 	sum = 0
 	num = num.to_s.split(//)
-	for i in 1..(num.size)
-		sum += num[i-1].to_i
-	end
+	num.size.times {|i|
+		sum += num[i].to_i
+	}
 	return sum
 end
 
