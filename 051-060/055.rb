5d1e9ca169f11da85292fc8f76a81c6ecea01593
Lycoris_i <some.chicken@gmail.com>
2013-08-02 19:33:33 +0900

    055 times used

--- 051-060/055.rb
+++ 051-060/055.rb
@@ -3,48 +3,48 @@
 #
 
 
-	def how_many_Lychrel(num)
-		answer = 0
-		for i in 1..num do
-			j = 1
-			p = i + rev_int(i)
-			if is_palindromic(p) then
+def how_many_Lychrel(num)
+	answer = 0
+	num.times {|i|
+		j = 1
+		p = (i + 1) + rev_int(i + 1)
+		if is_palindromic(p) then
+			answer += 1
+		else
+			while j < 50
+				p = p + rev_int(p)
+				if is_palindromic(p) then
 					answer += 1
-			else
-					while j < 50
-						p = p + rev_int(p)
-						if is_palindromic(p) then
-							answer += 1
-							break
-						end
-							j += 1
-					end
+					break
+				end
+				j += 1
 			end
 		end
-		answer = num - answer
-		return answer
-	end
+	}
+	answer = num - answer
+	return answer
+end
 		
 
-	def rev_int(num)
-		num = num.to_s.split(//)
-		num.reverse!
-		n = ""
-		for i in 0..(num.size - 1) do
-				n = n + num[i]
-		end
-		n = n.to_i
-		return n
-	end
+def rev_int(num)
+	num = num.to_s.split(//)
+	num.reverse!
+	n = ""
+	num.size.times {|i|
+		n = n + num[i]
+	}
+	n = n.to_i
+	return n
+end
 
 
-	def is_palindromic(num)
-		if num == rev_int(num)
-			return true
-		else
-			return false
-		end
+def is_palindromic(num)
+	if num == rev_int(num)
+		return true
+	else
+		return false
 	end
+end
 
 
 	puts "Answer: #{how_many_Lychrel(9999)}"	# Answer: 249
