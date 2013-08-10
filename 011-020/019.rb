100f8effbd104f409a9e4d257bc915caf4e2feab
Lycoris_i <some.chicken@gmail.com>
2013-08-10 23:55:00 +0900

    019 initial

---
+++ 011-020/019.rb
@@ -0,0 +1,20 @@
+# http://projecteuler.net/problem=19
+#
+#
+
+require 'date'
+
+def counting_sundays
+	a = 0
+	i = Date.new(1900, 12, 1)
+	1200.times {|k|
+		i = i >> 1
+		if i.sunday? == true then
+			a += 1
+		end
+	}
+	return a
+end
+
+
+	puts "Answer: #{counting_sundays}"
