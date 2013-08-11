6944617a612e5fb77855205a28dc904b9525d20f
Lycoris_i <some.chicken@gmail.com>
2013-08-11 20:08:28 +0900

    make triangle library

--- 011-020/018.rb
+++ 011-020/018.rb
@@ -2,33 +2,6 @@
 #
 #
 
-def maximum_path_sum(triangle)
-	f = open(triangle)
-	t = f.read.split(/\n/)
-	t.each_index{|i| t[i] = t[i].split(" ")}
-	t.each{|i|
-		i.each_index{|j|
-			i[j] = i[j].to_i
-		}
-	}
-	t.each_index{|i|
-		if t[i + 1] != nil then
-			t[i + 1].each_index{|k| 
-				if t[i][k - 1] != nil and t[i][k] != nil then
-					a = [t[i][k - 1], t[i][k]]
-					t[i + 1][k] += a.sort.last
-				elsif t[i][k - 1] == nil then
-					t[i + 1][k] += t[i][k]
-				else
-					t[i + 1][k] += t[i][k - 1]
-				end
-			}
-		end
-	}
-	p t
-	answer = t[t.size - 1].sort.last
-	return answer
-end
-
+require './triangle'
 
 	puts "Answer: #{maximum_path_sum("018.txt")}"		# Answer: 1074
