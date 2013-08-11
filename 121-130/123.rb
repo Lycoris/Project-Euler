a2bb75536ec296225959a28c4474ab4e3e52ef67
Lycoris_i <some.chicken@gmail.com>
2013-08-11 17:27:17 +0900

    123 initial

---
+++ 121-130/123.rb
@@ -0,0 +1,27 @@
+# http://projecteuler.net/problem=123
+#
+#
+
+require 'prime'
+
+
+def prime_square_remainders(b)
+	n = 0
+	Prime.each {|p|
+		n += 1
+		p n
+		if r(n, p) > b then
+			return n
+		end
+	}
+end
+
+
+def r(n, p)
+	a = (p - 1) ** n + (p + 1) ** n
+	a = a % (p ** 2)
+	return a
+end
+
+
+	puts "Answer: #{prime_square_remainders(10000000000)}"	# Answer: 21035
