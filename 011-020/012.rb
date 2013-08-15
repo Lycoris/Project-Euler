853f06c514dbdaa4342fd57badeb0982d044f439
Lycoris_i <some.chicken@gmail.com>
2013-08-15 17:56:20 +0900

    012 initial

---
+++ 011-020/012.rb
@@ -0,0 +1,27 @@
+# http://projecteuler.net/problem=12
+#
+#
+
+require 'prime'
+
+def highly_divisible(over)
+	n = 0
+	while true
+		n += 1
+		arr = triangular(n).prime_division
+		r = 1
+		arr.each {|a|
+			r *= a[1] + 1
+		}
+		break if r > (over - 1)
+	end
+	return triangular(n)
+end
+
+
+def triangular(n)
+	return (n * (n + 1)) / 2
+end
+
+
+	puts "Answer: #{highly_divisible(500)}"		# Answer: 76576500
