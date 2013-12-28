bbf8b15ee6d926fbfde86d4cfc15e3f168c5650a
Lycoris_i <some.chicken@gmail.com>
2013-12-28 20:15:44 +0900

    087 initial

---
+++ 081-090/087.rb
@@ -0,0 +1,37 @@
+# http://projecteuler.net/problem=87
+#
+#
+require 'prime'
+
+
+def prime_power_triples(l)
+	answer = Array.new
+	ary_a = Array.new
+		Prime.each {|a|
+			x = a ** 4 + 12
+			break if x >= l
+			ary_a << a
+		}
+	ary_b = Array.new
+	ary_a.reverse.each {|a|
+		Prime.each {|b|
+			x = a ** 4 + b ** 3 + 4
+			break if x >= l
+			ary_b << b
+		}
+	}
+	ary_b.uniq!
+	ary_a.reverse.each {|a|
+		ary_b.reverse.each {|b|
+			Prime.each {|c|
+				x = a ** 4 + b ** 3 + c ** 2
+				break if x >= l
+				answer << x
+			}
+		}
+	}
+	return answer.uniq.size
+end
+
+
+	puts "Answer: #{prime_power_triples(50000000)}"		# Answer: 1097343
