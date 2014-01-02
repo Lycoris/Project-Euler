b5ea7fb3ff32b9015ee36cd72a4850835f45067f
Lycoris_i <some.chicken@gmail.com>
2014-01-02 17:04:02 +0900

    015 initial

---
+++ 011-020/015.rb
@@ -0,0 +1,14 @@
+# http://projecteuler.net/problem=15
+#
+#
+
+def lattice_paths(x, y)
+	path = Array.new(x + 1).map!{Array.new}
+	x.times {|i| path[i + 1][0] = 1}
+	y.times {|i| path[0][i + 1] = 1}
+	x.times {|i| y.times {|j| path[i + 1][j + 1] = path[i][j + 1] + path[i + 1][j]}}
+	return path[x][y]
+end
+
+
+	puts "Answer: #{lattice_paths(20, 20)}"		# Answer: 137846528820
