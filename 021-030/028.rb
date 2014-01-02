3dd66c96463d7a0a2e774c49427df8514d49d26f
Lycoris_i <some.chicken@gmail.com>
2014-01-02 19:09:55 +0900

    028 initial

---
+++ 021-030/028.rb
@@ -0,0 +1,24 @@
+# http://projecteuler.net/problem=28
+#
+#
+
+def number_spiral(n)
+	i = -1
+	answer = 0
+	until i == n
+		i += 2
+		if i == 1 then
+			answer += 1
+			x = 1
+			next
+		end
+		4.times {|j|
+			x += i - 1
+			answer += x
+		}
+	end
+	return answer
+end
+
+
+	puts "Answer: #{number_spiral(1001)}"	# Answer: 669171001
