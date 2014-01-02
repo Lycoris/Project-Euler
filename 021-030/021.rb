96124f7f8e45f9feddb35ee759c88e0100cd2c68
Lycoris_i <some.chicken@gmail.com>
2014-01-02 18:35:03 +0900

    021 initial

---
+++ 021-030/021.rb
@@ -0,0 +1,28 @@
+# http://projecteuler.net/problem=21
+#
+#
+
+require 'prime'
+
+def amicable_numbers(n)
+	answer = 0
+	(n - 1).times {|i|
+		next if i == 0
+		answer += i + 1 if i + 1 == d(d(i + 1)) and i + 1 != d(i + 1)
+	}
+	return answer
+end
+
+
+def d(n)
+	d = 1
+	n.prime_division.each {|a|
+		r = 1
+		a[1].times {|b| r += a[0] ** (b + 1)}
+		d *= r
+	}
+	return d - n
+end
+
+
+	puts "Answer: #{amicable_numbers(10000)}"	# Answer: 31626
