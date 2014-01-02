44fe66ee80cf45a2cabe8ca9a193caea2c92f3b8
Lycoris_i <some.chicken@gmail.com>
2014-01-02 19:45:01 +0900

    036 initial

---
+++ 021-030/036.rb
@@ -0,0 +1,22 @@
+# http://projecteuler.net/problem=36
+#
+#
+
+def palindrome(n)
+	answer = 0
+	(n - 1).times {|i|
+		answer += i + 1 if is_rev(i + 1) == true and is_rev((i + 1).to_s(2)) == true
+	}
+	return answer
+end
+
+
+
+def is_rev(n)
+	a = n.to_s.split(//)
+	return true if a == a.reverse
+	return false
+end
+
+
+	puts "Answer: #{palindrome(1000000)}"	# Answer: 872187
