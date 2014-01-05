25692f9459c23870e324ca67157b785f6da72f76
Lycoris_i <some.chicken@gmail.com>
2014-01-06 02:46:37 +0900

    004 revised

--- 001-010/004.rb
+++ 001-010/004.rb
@@ -7,9 +7,7 @@ def largest_palindrome_product
 	for i in 100..999
 		for j in 100..999
 			n = i * j
-			if palindrome?(n) == true and n > a then
-				a = n
-			end
+			a = n if palindrome?(n) and n > a
 		end
 	end
 	return a
@@ -18,11 +16,8 @@ end
 
 def palindrome?(num)
 	s = num.to_s.split(//)
-	if s == s.reverse then
-		return true
-	else
-		return false
-	end
+	return true if s == s.reverse
+	return false
 end
 
 
