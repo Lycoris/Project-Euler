466947cffa404e6fb101442eafc94cf2334d1c34
Lycoris_i <some.chicken@gmail.com>
2014-01-06 02:33:37 +0900

    001 revised

--- 001-010/001.rb
+++ 001-010/001.rb
@@ -1,15 +1,11 @@
 # http://projecteuler.net/problem=1
 #
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-# Find the sum of all the multiples of 3 or 5 below 1000.
+#
+
 
 def sum_until(num)
 	total = 0
-	num.times {|n|
-		if (n + 1).modulo(3) == 0 or (n + 1).modulo(5) == 0 then
-			total += n + 1
-		end
-	}
+	num.times {|n| total += n + 1 if (n + 1).modulo(3) == 0 or (n + 1).modulo(5) == 0}
 	return total
 end
 
