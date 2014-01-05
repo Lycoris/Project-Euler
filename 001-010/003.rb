c9648ff128cdb8e4bb17e00c6062b1935f915370
Lycoris_i <some.chicken@gmail.com>
2014-01-06 02:40:15 +0900

    003 revised

--- 001-010/003.rb
+++ 001-010/003.rb
@@ -1,14 +1,7 @@
 # http://projecteuler.net/problem=3
 #
-# The prime factors of 13195 are 5, 7, 13 and 29.
-# What is the largest prime factor of the number 600851475143 ?
 #
-#
+
 require 'prime'
 
-def maxprime(num)
-		return Prime.prime_division(num).pop[0]
-end
-
-
-puts "Answer: #{maxprime(600851475143)}"	# Answer: 6857
+puts "Answer: #{Prime.prime_division(600851475143).pop[0]}"	# Answer: 6857
