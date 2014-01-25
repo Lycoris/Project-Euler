08b56eba5e32906ffa25e8e4bde32453f2b9b6ed
Lycoris_i <some.chicken@gmail.com>
2014-01-26 06:29:35 +0900

    053 initial

---
+++ 051-060/053.rb
@@ -0,0 +1,25 @@
+# http://projecteuler.net/problem=53
+#
+#
+
+require '../111-120/fact'
+
+def conbi(l)
+	answer = 0
+  99.times {|n|
+		n = n + 2
+		1.upto(n) {|r|
+      answer += 1 if c(n, r) > l
+		}
+	}
+  return answer
+end
+
+
+def c(n, r)
+  r = n - r if r > (n / 2)
+  return fact(n)/(fact(r) * fact(n - r))
+end
+
+
+puts "Answer: #{conbi(1000000)}"  # Answer: 4075
