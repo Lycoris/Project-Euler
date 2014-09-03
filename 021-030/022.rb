38263326d0762445b6a463cfa087aacfea240e69
Lycoris_i <some.chicken@gmail.com>
2014-09-03 23:02:39 +0900

    022 initial

---
+++ 021-030/022.rb
@@ -0,0 +1,23 @@
+# http://projecteuler.net/problem=22
+#
+#
+
+
+def name_score(file)
+  ans = 0
+  s = [("A".."Z").to_a, (1..26).to_a].transpose
+  s = Hash[*s.flatten]
+  f = open(file).read.delete('"').split(",").sort
+  f.each_index {|i|
+    score = 0
+    f[i].split(//).each {|l|
+      score += s[l]
+    }
+    ans += score * (i + 1)
+  }
+  return ans
+end
+
+
+  puts "Answer: #{name_score("name.txt")}"    # Answer: 871198282
+
