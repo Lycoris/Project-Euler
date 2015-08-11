6a9d4318bd2892a65f374719da887937ca45ccd6
Lycoris_i <some.chicken@gmail.com>
2015-08-11 10:23:34 +0900

    025 revised (used fib)

--- 021-030/025.rb
+++ 021-030/025.rb
@@ -2,23 +2,17 @@
 #
 #
 
+require '../lib/fib'
 
 def fibonacci_sequence_digit(x)
-	@fibonacci_sequence = {1 => 1, 2 => 1}
 	n = 2
 	while 1 > 0
 		n += 1
-		if Math.log10(fibonacci(n)).floor == x - 1 then
+		if Math.log10(fib(n)).floor == x - 1 then
 			return n
 		end
 	end
 end
 
 
-def fibonacci(n)
-	@fibonacci_sequence[n] = @fibonacci_sequence[n - 1] + @fibonacci_sequence[n - 2]
-	return @fibonacci_sequence[n]
-end
-
-
-	puts "Answer: #{fibonacci_sequence_digit(1000)}"
+	puts "Answer: #{fibonacci_sequence_digit(1000)}"  # Answer: 4782
