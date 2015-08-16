b42c1b310c7eefe1d72d518b973d4e9546858aa7
Lycoris_i <some.chicken@gmail.com>
2015-08-16 17:37:12 +0900

    010 revised

--- 001-010/010.rb
+++ 001-010/010.rb
@@ -5,14 +5,4 @@
 
 require 'prime'
 
-
-def summation_of_primes(x)
-	a = 0
-	Prime.each(x) {|prime|
-		a += prime
-	}
-	return a
-end
-
-
-puts "Answer: #{summation_of_primes(2000000)}"		# Answer: 142913828922
+puts "Answer: #{Prime.each(2000000).inject(:+)}"		# Answer: 142913828922
